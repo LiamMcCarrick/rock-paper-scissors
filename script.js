2a1985e37c8b0a0ee0b41a94988e35e34101af3a
Liam McCarrick <129102185+LiamMcCarrick@users.noreply.github.com>
2025-01-13 19:29:07 -0500

    create computer choice function

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+function getComputerChoice() {
+    let compChoice = +Math.random().toFixed(3);
+    if (compChoice < .333) {
+        return "Rock";
+    } else if (compChoice >= .333 && compChoice < .667) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
+
+console.log(getComputerChoice());
